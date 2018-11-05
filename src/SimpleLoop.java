a8263d6080597de62aff35b33a0f59f6528cbd5b
heso6 <romaniuk99@gmail.com>
2018-11-05 16:36:57 +0100

    Initial commit!

--- src/SimpleLoop.java
+++ src/SimpleLoop.java
@@ -1,6 +1,10 @@
 public class SimpleLoop {
     public static void main(String[] args) {
 
+        for (int i = 10; i <=20; i++){
+            System.out.println(i);
+        }
+
         System.out.println("Hello World!");
     }
 }
